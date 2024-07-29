711884c6c6a1b9e0658528da674a846199e6f6ee
sarth-shr <126249666+sarth-shr@users.noreply.github.com>
2024-07-29 14:30:55 +0545

    Implement wild card routing for 404 page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import { CustomerAccountComponent } from './customer-account/customer-account.component';
 import { CustomerProfileComponent } from './customer-profile/customer-profile.component';
 import { CustomerUpdatePersonalComponent } from './customer-update-personal/customer-update-personal.component';
+import { CustomerUpdateSecurityComponent } from './customer-update-security/customer-update-security.component';
 import { authGuard } from './guards/auth.guard';
 import { LoginComponent } from './login/login.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { RegisterComponent } from './register/register.component';
 
 export const routes: Routes = [
@@ -30,14 +32,25 @@ export const routes: Routes = [
     canActivate: [authGuard],
   },
   {
-    path: 'profile/update',
+    path: 'profile/update-personal',
     title: 'Update Personal Information',
     component: CustomerUpdatePersonalComponent,
-    canActivate: [authGuard]
+    canActivate: [authGuard],
+  },
+  {
+    path: 'profile/update-security',
+    title: 'Update Security Information',
+    component: CustomerUpdateSecurityComponent,
+    canActivate: [authGuard],
   },
   {
     path: '',
     redirectTo: 'profile',
     pathMatch: 'full',
+  },
+  {
+    path: '**',
+    title: 'ERROR 404: NOT FOUND',
+    component: PageNotFoundComponent,
   },
 ];
