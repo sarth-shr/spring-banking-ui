b2b0b73682c7bc2d48f9242dc8f1c02389bd6bad
sarth-shr <126249666+sarth-shr@users.noreply.github.com>
2024-07-29 20:43:14 +0545

    Implement cross field password validation in template

--- src/app/validators/password-validator.ts
+++ src/app/validators/password-validator.ts
@@ -1,8 +1,14 @@
 import { AbstractControl } from '@angular/forms';
 
-export function PasswordValidator(control: AbstractControl): { [key: string]: boolean } | null {
+export function PasswordValidator(
+  control: AbstractControl
+): { [key: string]: boolean } | null {
   const newPassword = control.get('newPassword');
   const confirmNewPassword = control.get('confirmNewPassword');
+
+  if (confirmNewPassword?.hasError('required') || confirmNewPassword?.pristine) {
+    return null;
+  }
 
   return newPassword &&
     confirmNewPassword &&
